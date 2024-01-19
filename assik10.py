73fcd97f2ab545687f8d7ce89baeed53583473b6
Dilnazamanzholova22052006 <154108941+Dilnazamanzholova22052006@users.noreply.github.com>
2024-01-19 11:03:02 +0600

    Update assik10.py

--- assik10.py
+++ assik10.py
@@ -1,6 +1,7 @@
-n=int(input())
-fib=[1,1]
-while len(fib)<n:
-    next_num=fib[-1]+fib[-2]
-    fib.append(next_num)
-print(fib[:n])
+def task14():
+    n = int(input())
+    fibbonaci = [1, 1]
+    for i in range(n - 2):
+        fibbonaci.append(fibbonaci[len(fibbonaci) - 1] + fibbonaci[len(fibbonaci) - 2])
+    for number in fibbonaci:
+        print(number, end=" ")
